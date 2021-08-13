4a0b5d11417fb65d417df099de96f169221a02f0
Ayo <bimboladxtreme@gmail.com>
2021-08-13 23:32:55 +0100

    completed signIn screen

--- src/components/SignUpScreen.js
+++ src/components/SignUpScreen.js
@@ -5,10 +5,15 @@ function SignUpScreen() {
 	return (
 		<Div>
 			<form>
-				<h1>Sign In</h1>
+				<h2>Sign In</h2>
 				<input type="email" placeholder="Email" />
 				<input type="password" placeholder="Password" />
 				<button type="submit">Sign In</button>
+				<h4>
+					<span className="gray__text">New to Netflix? &nbsp;</span>
+
+					<span className="link__text">Sign up Now.</span>
+				</h4>
 			</form>
 		</Div>
 	);
@@ -18,14 +23,19 @@ export default SignUpScreen;
 
 const Div = styled.div`
 	max-width: 300px;
-	padding: 70px;
+	padding: 40px 50px;
 	margin-left: auto;
 	margin-right: auto;
 	background: rgba(0, 0, 0, 0.85);
+	margin-top: -30px;
 
 	form {
 		display: grid;
 		flex-direction: column;
+
+		h2 {
+			margin-bottom: 10px !important;
+		}
 
 		input {
 			padding: 5px 10px;
@@ -33,6 +43,33 @@ const Div = styled.div`
 			height: 40px;
 			margin-bottom: 14px;
 			border: none;
+			border-radius: 5px;
+		}
+
+		button {
+			padding: 16px 20px;
+			font-size: 1rem;
+			border: none;
+			font-weight: 600;
+			background: #e50914;
+			cursor: pointer;
+			color: #fff;
+			margin-top: 10px;
+			border-radius: 5px;
+		}
+
+		h4 {
+			text-align: left;
+			margin-top: 20px;
+		}
+
+		.gray__text {
+			color: gray;
+		}
+
+		.link__text:hover {
+			cursor: pointer;
+			text-decoration: underline;
 		}
 	}
 `;
