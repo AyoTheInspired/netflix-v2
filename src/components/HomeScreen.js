3f458302029fb9b393fbd253bb21b75e1b50cdd3
Ayo <bimboladxtreme@gmail.com>
2021-08-13 20:40:13 +0100

    Rows are complete

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import requests from "../requests";
+import Row from "./Row";
 import Banner from "./Banner";
 import Nav from "./Nav";
 
@@ -8,6 +10,18 @@ function HomeScreen() {
 		<Div>
 			<Nav />
 			<Banner />
+			<Row
+				title="NETFLIX ORIGINALS"
+				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow
+			/>
+			<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+			<Row title="Action" fetchUrl={requests.fetchTopRated} />
+			<Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />{" "}
 		</Div>
 	);
 }
---
+++ src/components/Row.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "../axios";
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+	const [movies, setMovies] = useState([]);
+
+	const base_url = `https://image.tmdb.org/t/p/original/`;
+
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchUrl);
+			setMovies(request.data.results);
+			return request;
+		}
+
+		fetchData();
+	}, [fetchUrl]);
+
+	return (
+		<Div>
+			<h2>{title}</h2>
+			<div className="row__posters">
+				{movies.map(
+					(movie, id) =>
+						((isLargeRow && movie.poster_path) ||
+							(!isLargeRow && movie.backdrop_path)) && (
+							<img
+								className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+								src={`${base_url}${
+									isLargeRow ? movie.poster_path : movie.backdrop_path
+								}`}
+								alt={movie.id}
+								key={id + 1}
+							/>
+						)
+				)}
+			</div>
+		</Div>
+	);
+}
+
+export default Row;
+
+const Div = styled.div`
+	color: #fff;
+	margin-left: 20px;
+
+	.row__posters {
+		display: flex;
+		overflow-y: hidden;
+		overflow-x: scroll;
+		padding: 20px;
+	}
+
+	.row__posters::-webkit-scrollbar {
+		display: none;
+	}
+
+	.row__poster {
+		max-height: 100px;
+		object-fit: contain;
+		margin-right: 10px;
+		width: 100%;
+		transition: transform 450ms;
+
+		&:hover {
+			transform: scale(1.08);
+			box-shadow: 0px -1px 1px rgba(255, 255, 255, 0.9);
+		}
+	}
+
+	.row__posterLarge {
+		max-height: 250px;
+		margin-right: 5px;
+		cursor: pointer;
+
+		&:hover {
+			transform: scale(1.09);
+			box-shadow: 0px -2px 5px rgba(255, 255, 255, 0.9);
+		}
+	}
+`;
